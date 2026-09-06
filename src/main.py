dad100a77845ddcc26bc528790e69a76922d9414
bigiraneza-car <b.igiraneza@alustudent.com>
2026-09-06 14:01:14 +0200

    Added the first part importing the raw text

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+#importing python's regex module
 import re
-import raw-text
+from pathlib import Path
+#Use python's built-in variable to locate the project root for the program to work.
+ROOT = Path(__file__).resolve().parent.parent
+Input_file = ROOT / "input" / "raw-text.txt"
+#Take the entire raw_text as a single string, with utf-8 to read the non_ASCII characters.
+text =Input_file.read_text(encoding="utf-8")
