ff7bbb19876f535248aa4efe3199b1328d4f57ea
bigiraneza-car <b.igiraneza@alustudent.com>
2026-09-06 16:07:28 +0200

    time extraction and emails extraction draft

--- src/main.py
+++ src/main.py
@@ -14,3 +14,11 @@ print(f"what is the length of the input text: {len(text)}")
 #phone number extraction
 for phone in re.findall(r"\(?(?:\b|\+)\d{1,3}\)?[\s-]\(?\d{2,4}\)?[-\s]?\d{2,4}[-\s]?\d{2,4}\b", text):
     print(phone)
+
+#Time extraction, format 24-h
+for time in re.findall(r"\b(?:[0-1]\d|2[0-3]):[0-5]\d\b", text):
+    print(time)
+
+#emails extraction
+for email in re.finditer(r"\b[a-zA-Z0-9\-._+%]+@([a-zA-Z0-9\.\-]+.com|alueducation.com|alumni.alueducation.com|si.alueducation.com)\b", text):
+    print(email.group())
