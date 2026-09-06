39961f14627b75df98abbd0b8767d4ae6694cbd1
bigiraneza-car <b.igiraneza@alustudent.com>
2026-09-06 19:09:52 +0200

    extracted the credit card number

--- src/main.py
+++ src/main.py
@@ -10,21 +10,26 @@ text =Input_file.read_text(encoding="utf-8")
 #treats the input as untrustworthy
 
 #Patterns for extractions
-Email = re.compile(r"\b[a-zA-Z0-9\-._+%]+@[a-zA-Z0-9\.\-]+\.com\b", text, re.I)  #Username:letters, digits,dot, hyphen or _ @ domain: letters, -, .
+Email = re.compile(r"\b[a-zA-Z0-9\-._+%]+@[a-zA-Z0-9\.\-]+\.com\b", re.I)  #Username:letters, digits,dot, hyphen or _ @ domain: letters, -, .
 
-Phone = re.compile(r"\(?(?:\b|\+)\d{1,3}\)?[\s-]\(?\d{2,4}\)?[-\s]?\d{2,4}[-\s]?\d{2,4}\b", text) #lengths 7to15, +(optional), countrycode, separators: whitespace, -, (), or no separator
+Phone = re.compile(r"\(?(?:\b|\+)\d{1,3}\)?[\s-]\(?\d{2,4}\)?[-\s]?\d{2,4}[-\s]?\d{2,4}\b") #lengths 7to15, +(optional), countrycode, separators: whitespace, -, (), or no separator
 
-Time = re.compile(r"\b(?:[0-1]\d|2[0-3]):[0-5]\d\b", text)  #24-h format: 00:00 to 23:59. hour(1):from 0 to 2, 0 to 9. Minutes: from 0 to 5, 0 to 9.
+Time = re.compile(r"\b(?:[0-1]\d|2[0-3]):[0-5]\d\b")  #24-h format: 00:00 to 23:59. hour(1):from 0 to 2, 0 to 9. Minutes: from 0 to 5, 0 to 9.
 
-Card:
+Card = re.compile(r"\b(\d{1,6}[-\s]?){4}")  # 4 groups of 4 digits each, or 5 or 6.
+
 #phone number extraction.
-for phone in re.findall(r"\(?(?:\b|\+)\d{1,3}\)?[\s-]\(?\d{2,4}\)?[-\s]?\d{2,4}[-\s]?\d{2,4}\b", text):
-    print(phone)
+for p in Phone.finditer(text):
+    print(p.group())
 
 #Time extraction, format 24-h
-for time in re.findall(r"\b(?:[0-1]\d|2[0-3]):[0-5]\d\b", text):
-    print(time)
+for t in Time.finditer(text):
+    print(t.group())
 
 #emails extraction
-for email in re.finditer(r"\b[a-zA-Z0-9\-._+%]+@[a-zA-Z0-9\.\-]+\.com\b", text, re.I):
-    print(email.group())
+for e in Email.finditer(text, re.I):
+    print(e.group())
+
+#credit card number extraction
+for c in Card.finditer(text):
+    print(c.group())
